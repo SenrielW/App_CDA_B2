e613ece6458e4087f7512cd1ca997a4a6ba925cc
Wilgenbus <robinwilgenbus@gmail.com>
2023-01-31 10:36:52 +0100

    Associate button

--- app/src/main/java/com/example/app_cda_b2/MainActivity.kt
+++ app/src/main/java/com/example/app_cda_b2/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.app_cda_b2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recup le bouton
+        val button = findViewById<Button>(R.id.start_new_activity_btn)
+        button.setOnClickListener {
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
